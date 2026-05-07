f18bc4fdfd9e2f1d78edcab666a55020ad7d9014
TAEYUN CHOI <yooni0215@naver.com>
2026-05-07 13:49:43 +0900

    [Passed] 세 실수의 반올림 (9ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a; float b; float c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl << b << endl << c;
+    return 0;
+}
