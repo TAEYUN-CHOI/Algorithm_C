999ab07d77cdb4949d67ff23a9b4cad0dca265bc
TAEYUN CHOI <yooni0215@naver.com>
2026-05-07 16:16:18 +0900

    [Passed] 문자열 출력하기 (9ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i; int k;
+    cin >> k;
+    for(i=0; i<k; i++){
+        cout << "LeebrosCode" << endl;
+    }
+    return 0;
+}
